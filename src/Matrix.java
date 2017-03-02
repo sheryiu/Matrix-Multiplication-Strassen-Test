edfb52ca8631e54bf65e8352f8e6560b3cbe260d
Yiu Cheuk Lok <yiu.cheuk.lok@gmail.com>
2017-03-02 22:14:22 +0800

    Changed matrix operations

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,8 +1,8 @@
 
 public class Matrix {
 	
-	private int[][] data;
-	private int size;
+	public int[][] data;
+	public int size;
 
 	public Matrix(int size) {
 		data = new int[size][size];
@@ -12,14 +12,6 @@ public class Matrix {
 	public Matrix(int size, int[][] matrix) {
 		this(size);
 		data = matrix;
-	}
-	
-	public int getEntry(int i, int j) {
-		return data[i][j];
-	}
-	
-	public int getSize() {
-		return size;
 	}
 	
 	public void setEntry(int value, int i, int j) {
@@ -64,35 +56,35 @@ public class Matrix {
 	}
 	
 	public static Matrix addition(Matrix a, Matrix b) {
-		int[][] temp = new int[a.getSize()][a.getSize()];
-		for (int i=0; i<a.getSize(); i++) {
-			for (int j=0; j<a.getSize(); j++) {
-				temp[i][j] = a.getEntry(i, j)+b.getEntry(i, j);
+		int[][] temp = new int[a.size][a.size];
+		for (int i=0; i<a.size; i++) {
+			for (int j=0; j<a.size; j++) {
+				temp[i][j] = a.data[i][j]+b.data[i][j];
 			}
 		}
-		return new Matrix(a.getSize(), temp);
+		return new Matrix(a.size, temp);
 	}
 	
 	public static Matrix subtraction(Matrix a, Matrix b) {
-		int[][] temp = new int[a.getSize()][a.getSize()];
-		for (int i=0; i<a.getSize(); i++) {
-			for (int j=0; j<a.getSize(); j++) {
-				temp[i][j] = a.getEntry(i, j)-b.getEntry(i, j);
+		int[][] temp = new int[a.size][a.size];
+		for (int i=0; i<a.size; i++) {
+			for (int j=0; j<a.size; j++) {
+				temp[i][j] = a.data[i][j]-b.data[i][j];
 			}
 		}
-		return new Matrix(a.getSize(), temp);
+		return new Matrix(a.size, temp);
 	}
 	
 	public static Matrix multiplication(Matrix a, Matrix b) {
-		int[][] temp = new int[a.getSize()][a.getSize()];
-		for (int i=0; i<a.getSize(); i++) {
-			for (int j=0; j<a.getSize(); j++) {
-				for (int k=0; k<a.getSize(); k++) {
-					temp[i][j] += a.getEntry(i, k)*b.getEntry(k, j);
+		int[][] temp = new int[a.size][a.size];
+		for (int i=0; i<a.size; i++) {
+			for (int j=0; j<a.size; j++) {
+				for (int k=0; k<a.size; k++) {
+					temp[i][j] += a.data[i][k]*b.data[k][j];
 				}
 			}
 		}
-		return new Matrix(a.getSize(), temp);
+		return new Matrix(a.size, temp);
 	}
 	
 	public static Matrix mergeBisection(int size, Matrix[] c) {
@@ -101,16 +93,16 @@ public class Matrix {
 			for (int j=0; j<size; j++) {
 				if (i < size/2 && j < size/2) {
 					// top left submatrix --> [0]
-					temp[i][j] = c[0].getEntry(i, j);
+					temp[i][j] = c[0].data[i][j];
 				} else if (i >= size/2 && j < size/2) {
 					// bottom left submatrix --> [2]
-					temp[i][j] = c[2].getEntry(i-size/2, j);
+					temp[i][j] = c[2].data[i-size/2][j];
 				} else if (i < size/2 && j >= size/2) {
 					// top right submatrix --> [1]
-					temp[i][j] = c[1].getEntry(i, j-size/2);
+					temp[i][j] = c[1].data[i][j-size/2];
 				} else if (i >= size/2 && j >= size/2) {
 					// bottom right submatrix --> [3]
-					temp[i][j] = c[3].getEntry(i-size/2, j-size/2);
+					temp[i][j] = c[3].data[i-size/2][j-size/2];
 				}
 			}
 		}
